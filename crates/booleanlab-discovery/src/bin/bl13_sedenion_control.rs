e16186202e4f6a113dc0077a1f8d82b0d4656e1e
MEMOPERF <contact@checkupauto.fr>
2026-09-12 18:15:29 +0200

    feat: add BL-13 sedenion predicate control runner

---
+++ crates/booleanlab-discovery/src/bin/bl13_sedenion_control.rs
@@ -0,0 +1,36 @@
+use booleanlab_discovery::DedupIndex;
+use booleanlab_discovery::sedenion::control_component_functions;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let functions = control_component_functions()?;
+    let mut exact_index = DedupIndex::new();
+    let mut complement_index = DedupIndex::new();
+
+    println!(
+        "coordinate\tdegree\tnonlinearity\tbalanced\tbent\tcorrelation_immunity\tfingerprint"
+    );
+
+    for (coordinate, function) in functions.into_iter().enumerate() {
+        let metrics = function.exact_metrics();
+        let fingerprint = function.stable_fingerprint();
+        let canonical = function.canonical_under_complement();
+        let _ = exact_index.insert(function);
+        let _ = complement_index.insert(canonical);
+        println!(
+            "{coordinate}\t{}\t{}\t{}\t{}\t{}\t{:016x}",
+            metrics.algebraic_degree,
+            metrics.nonlinearity,
+            metrics.balanced,
+            metrics.bent,
+            metrics.correlation_immunity,
+            fingerprint
+        );
+    }
+
+    println!(
+        "summary\texact_unique={}\tcomplement_unique={}",
+        exact_index.unique_functions(),
+        complement_index.unique_functions()
+    );
+    Ok(())
+}
