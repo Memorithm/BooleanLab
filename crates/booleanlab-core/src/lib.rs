47e8ece597d426593ca2e9222c3340436878d5ae
MEMOPERF <contact@checkupauto.fr>
2026-09-12 22:20:56 +0200

    feat(bme): export canonical equation API

--- crates/booleanlab-core/src/lib.rs
+++ crates/booleanlab-core/src/lib.rs
@@ -1,11 +1,13 @@
 #![forbid(unsafe_code)]
 
 pub mod bme;
+pub mod bme_equation;
 pub mod circuit;
 pub mod hybrid;
 pub mod state;
 
 pub use bme::{BmeError, or_and_cell, thresholded_xnor_cell, xnor_popcount_cell, xor_and_cell};
+pub use bme_equation::{CanonicalBmeEquation, CanonicalBmeOutput};
 pub use circuit::{BooleanCircuit, CircuitError, Node, NodeId, TruthRow};
 pub use hybrid::{HybridOperator, PredicateBridge};
 pub use state::{BitState, StateError};
