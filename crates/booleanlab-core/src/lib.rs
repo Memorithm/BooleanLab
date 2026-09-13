c54b312ee42f0cba8b173490f8099b69aa55d7eb
MEMOPERF <contact@checkupauto.fr>
2026-09-13 11:35:40 +0200

    fix(bme): export checked cost accounting

--- crates/booleanlab-core/src/lib.rs
+++ crates/booleanlab-core/src/lib.rs
@@ -1,12 +1,14 @@
 #![forbid(unsafe_code)]
 
 pub mod bme;
+pub mod bme_cost;
 pub mod bme_equation;
 pub mod circuit;
 pub mod hybrid;
 pub mod state;
 
 pub use bme::{BmeError, or_and_cell, thresholded_xnor_cell, xnor_popcount_cell, xor_and_cell};
+pub use bme_cost::{BmeCostError, BmeLogicalCost, BmeShape, logical_cost};
 pub use bme_equation::{CanonicalBmeEquation, CanonicalBmeOutput};
 pub use circuit::{BooleanCircuit, CircuitError, Node, NodeId, TruthRow};
 pub use hybrid::{HybridOperator, PredicateBridge};
